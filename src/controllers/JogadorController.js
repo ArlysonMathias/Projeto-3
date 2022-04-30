b67a47f506596529e6e5828f5a64bca5ccddc616
Arlyson Mathias <mathiassouza10@outlook.com>
2022-04-30 15:13:22 -0300

    segundo commit

--- src/controllers/JogadorController.js
+++ src/controllers/JogadorController.js
@@ -1,9 +1,10 @@
-import { Jogador } from "../models/Jogador.js";
+// import { Sequelize } from "sequelize";
+import {Jogador} from "../models/Jogador.js";
 
 export const getAll = async (req, res) => {
   try {
     const copaMundo = await Jogador.findAll();
-    res.render("index", { copaMundo, jogador: undefined });
+    res.render("index", { copaMundo,jogador: undefined });
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
