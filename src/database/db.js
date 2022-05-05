1cb5d2e63aabcde9f49417eb5979c876aea6ca6d
Arlyson Mathias <mathiassouza10@outlook.com>
2022-05-05 19:58:24 -0300

    atualização na conect

--- src/database/db.js
+++ src/database/db.js
@@ -5,27 +5,27 @@ dotenv.config();
 
 // config para subir deploy
 
-// export const connection = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+export const connection = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 
 //config para rodar localmente
 
 
-export const connection = new Sequelize(
-  process.env.DB_BASE,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: 5432,
-    dialect: "postgres",
-  }
-);
+// export const connection = new Sequelize(
+//   process.env.DB_BASE,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     port: 5432,
+//     dialect: "postgres",
+//   }
+// );
